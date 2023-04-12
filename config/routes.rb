9ef895fd6670f0c0f1f259867d85623a625ac0df
Giulia Zeni <giuli.zeni@gmail.com>
2023-04-12 14:19:01 +0200

    added profile page to frontend potato, need to fix css and replace tags with classes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'newspaper', to: 'pages#newspaper', as: :newspaper
+  get 'profile_page', to: 'pages#profile_page', as: :profile_page
 end
